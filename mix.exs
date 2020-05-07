ed1530651772fc3a7a34dc208840d89dc86f8a8a
Alvaro Vilanova <alvivi@gmail.com>
2020-05-07 19:00:27 +0200

    Add code coverage reporting

--- mix.exs
+++ mix.exs
@@ -7,21 +7,38 @@ defmodule PlugAmqp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: test_coverage(),
+      preferred_cli_env: preferred_cli_env()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:propcheck, "~> 1.2"}
+      {:excoveralls, "~> 0.12", only: :test},
+      {:propcheck, "~> 1.2", only: :test}
+    ]
+  end
+
+  defp test_coverage do
+    [
+      tool: ExCoveralls
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.github": :test,
+      "coveralls.html": :test,
+      "coveralls.post": :test
     ]
   end
 end
